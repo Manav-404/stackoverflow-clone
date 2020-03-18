5a2dced8206c757abc92cca9268da6e280ee6fbf
Manv Desai <46566032+Manav-404@users.noreply.github.com>
2020-03-18 16:32:09 +0000

    Update connectionurl.js

--- setup/connectionurl.js
+++ setup/connectionurl.js
@@ -1,4 +1,4 @@
 module.exports = {
-mongoURL :'mongodb+srv://manav:!Manav13@course-fpfwu.mongodb.net/test?retryWrites=true&w=majority',
+mongoURL :'mongodb+srv://username:yourpassword@course-fpfwu.mongodb.net/test?retryWrites=true&w=majority',
 secret : 'stacksecretconn',
 };
